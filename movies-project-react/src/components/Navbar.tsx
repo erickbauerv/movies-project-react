4710241ebce391136263f8e41be33f2d2fe497f7
erickbauer <contatoerickbauer@gmail.com>
2024-03-31 18:41:11 -0300

    Update Navbar and index.css

--- movies-project-react/src/components/Navbar.tsx
+++ movies-project-react/src/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
 function Navbar() {
   return (
     <nav id="navbar">
       <h2>
-        <Link to="/">MovieLib</Link>
+        <Link to="/">
+          <BiCameraMovie/> MoviesLib
+        </Link>
       </h2>
-      <Link to="/movie/1">Movie</Link>
-      <Link to="/search">Search</Link>
+      <form>
+        <input type="text" placeholder="Busque um filme"/>
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
     </nav>
   )
 }
