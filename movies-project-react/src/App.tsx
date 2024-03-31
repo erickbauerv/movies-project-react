f58b1a1df719fa80a1b8860e89f6ab3f4551298c
erickbauer <contatoerickbauer@gmail.com>
2024-03-31 17:53:35 -0300

    Implementing navbar.

--- movies-project-react/src/App.tsx
+++ movies-project-react/src/App.tsx
@@ -1,6 +1,19 @@
+import { Link, Outlet } from 'react-router-dom';
+import "./App.css";
+
 function App() {
   return (
-    <div>App</div>
+    <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">MovieLib</Link>
+        </h2>
+        <Link to="/movie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <h2>Movie Lib</h2>
+      <Outlet/>
+    </div>
   )
 }
 
