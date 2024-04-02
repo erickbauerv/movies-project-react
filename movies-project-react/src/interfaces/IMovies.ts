f5400ff265778d3db51191ee41ab792cd985d6be
erickbauer <contatoerickbauer@gmail.com>
2024-04-02 17:42:15 -0300

    Implemented communication with the API

---
+++ movies-project-react/src/interfaces/IMovies.ts
@@ -0,0 +1,3 @@
+export interface IMovie {
+  title: string
+}
--- movies-project-react/src/pages/Home.tsx
+++ movies-project-react/src/pages/Home.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { IMovie } from '../interfaces/IMovies';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 function Home() {
+  const [topMovies, setTopMovies] = useState<IMovie[]>([]);
+
+  async function getTopRatedMovies(url: string){
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl);
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+    </div>
   )
 }
 
