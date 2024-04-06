d7f12383e4f4575017f4a23a3419fb88a2911da9
erickbauer <contatoerickbauer@gmail.com>
2024-04-06 14:08:45 -0300

    Implemented Search page.

--- movies-project-react/src/pages/Search.tsx
+++ movies-project-react/src/pages/Search.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { IMovie } from '../interfaces/IMovie';
+import MovieCard from "../components/MovieCard";
+import "../styles/MoviesGrid.css";
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 function Search() {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
+
+  async function getSearchedMovies(url: string) {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  }
+
   return (
-    <div>Search</div>
+    <div className="container">
+      <h2 className="title">
+        Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
   )
 }
 
