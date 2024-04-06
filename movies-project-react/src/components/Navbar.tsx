a29cae21d0f48c2c42853ab82844dede36513a8a
erickbauer <contatoerickbauer@gmail.com>
2024-04-06 11:14:00 -0300

    Updated Navbar.tsx

--- movies-project-react/src/components/Navbar.tsx
+++ movies-project-react/src/components/Navbar.tsx
@@ -1,8 +1,21 @@
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 import '../styles/Navbar.css';
 
 function Navbar() {
+  const [search, setSearch] = useState<string>("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>): undefined {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+
   return (
     <nav id="navbar">
       <h2>
@@ -10,8 +23,13 @@ function Navbar() {
           <BiCameraMovie/> MoviesLib
         </Link>
       </h2>
-      <form>
-        <input type="text" placeholder="Busque um filme"/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={e => setSearch(e.target.value)}
+          value={search}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
